d5c3d66703f7474de3813f8b3d9161e89db8752e
Gustavo Almeida de Jesus <66255218+SystemGuuh@users.noreply.github.com>
2024-05-08 12:23:24 +0000

    build: edit pages names

--- Home.py
+++ Home.py
@@ -1,9 +1,4 @@
 import streamlit as st
-from utils.monday import get_mysql_connection, getDfFromQuery
-from utils.queries import GET_RADAR_FROM_BD
-import threading
-from datetime import datetime
-import pytz
 
 #colocar para printar o horário e dia da ultima atualização do bd
 def run():
@@ -16,15 +11,7 @@ def run():
     col2.image("./assets/imgs/eshows-logo.png", width=100)
     col1.write("# Radar de Implementação")
 
-    st.markdown(
-        """
-        #### Visualização dos dados do Radar de Implementação:
-        - **Visão Geral do Monday:** Página para visualizar dados coletados do Radar.
-        - **Hunter:** Página para visualizar dados pertinentes a Hunters.
-        - **Farmer:** Página para visualizar dados pertinentes a Farmers.
-        - **Implementação:** Página para visualizar dados de implantantação.
-        """
-    )
+    st.markdown("# Dados da empresa")
 
 if __name__ == "__main__":
     run()
