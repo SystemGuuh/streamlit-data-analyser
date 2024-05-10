6056de09e6a4813058d20f5419a0b06815f2fb8f
Gustavo Almeida de Jesus <66255218+SystemGuuh@users.noreply.github.com>
2024-05-10 18:23:36 +0000

    refactor: remove not used try and catch

--- pages/Home.py
+++ pages/Home.py
@@ -15,55 +15,51 @@ hide_sidebar()
 if 'loggedIn' not in st.session_state:
     st.switch_page("main.py")
 
-try:
-    if st.session_state['loggedIn']:
-    
-        # Define ID
-        id = 31582
+if st.session_state['loggedIn']:
 
-        # Header
-        col1, col2 = st.columns([4,1])
+    # Define ID
+    id = 31582
 
-        # Caso não ache o ID de usuário
-        username = GET_USER_NAME(id)
-        if username.empty:
-            col1.error('ID de usuário não encontrado')
-        else:
-            col1.write(f"## Olá, {username.iloc[0]['FULL_NAME']}")
-        col2.image("./assets/imgs/eshows-logo.png", width=100)
-        col2.button('Logout', key='Logout', on_click=logout())
-        
+    # Header
+    col1, col2 = st.columns([4,1])
 
-        # Nav
-        st.divider()
-        col4, col5, col6 = st.columns([1, 3, 1])
-        with col4:
-            inputDate = filterCalendarComponent()
-        col5.markdown("<h3 style='text-align: center;'>Dash Empresa - Resumo</h3>", unsafe_allow_html=True)
-        with col6:
-            inputEstablishment = filterEstablishmentComponent(id)
-        df = getDataframeDashGeral(id, inputDate, inputEstablishment) 
-
-        # Dataframe vazio
-        if df.empty:
-            col1.warning('Parace que você não permissão para visualizar dados de casas.')
-
-        #Body
-        tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["DASH GERAL", "DASH ANALÍTICO CORPORATIVO MENSAL", "DASH TEMPORAL", "DASH ANALÍTICO", "DASH POR CASA", "CONTROLE DE LANÇAMENTOS"])
-        with tab1:
-            buildGeneralDash(df)
-        with tab2:
-            buildCorporativeDash(df)
-        with tab3:
-            buildTemporalDash(df)
-        with tab4:
-            buildAnaliticsDash(df)
-        with tab5:
-            buildByHouseDash(df)
-        with tab6:
-            buildReleaseControl(df)
+    # Caso não ache o ID de usuário
+    username = GET_USER_NAME(id)
+    if username.empty:
+        col1.error('ID de usuário não encontrado')
     else:
-        st.switch_page("main.py")
+        col1.write(f"## Olá, {username.iloc[0]['FULL_NAME']}")
+    col2.image("./assets/imgs/eshows-logo.png", width=100)
+    col2.button('Logout', key='Logout', on_click=logout())
+    
 
-except KeyError:
+    # Nav
+    st.divider()
+    col4, col5, col6 = st.columns([1, 3, 1])
+    with col4:
+        inputDate = filterCalendarComponent()
+    col5.markdown("<h3 style='text-align: center;'>Dash Empresa - Resumo</h3>", unsafe_allow_html=True)
+    with col6:
+        inputEstablishment = filterEstablishmentComponent(id)
+    df = getDataframeDashGeral(id, inputDate, inputEstablishment) 
+
+    # Dataframe vazio
+    if df.empty:
+        col1.warning('Parace que você não permissão para visualizar dados de casas.')
+
+    #Body
+    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["DASH GERAL", "DASH ANALÍTICO CORPORATIVO MENSAL", "DASH TEMPORAL", "DASH ANALÍTICO", "DASH POR CASA", "CONTROLE DE LANÇAMENTOS"])
+    with tab1:
+        buildGeneralDash(df)
+    with tab2:
+        buildCorporativeDash(df)
+    with tab3:
+        buildTemporalDash(df)
+    with tab4:
+        buildAnaliticsDash(df)
+    with tab5:
+        buildByHouseDash(df)
+    with tab6:
+        buildReleaseControl(df)
+else:
     st.switch_page("main.py")
