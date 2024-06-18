329ff2c89f2919ab6425c3b3d15459f680f13545
Gustavo Almeida <gustavo.jesus2806@gmail.com>
2024-06-18 11:24:08 -0300

    fix: separete func to call querys

--- data/get_data.py
+++ data/get_data.py
@@ -1,5 +1,6 @@
 from data.dbconnect import *
 from utils.functions import *
+import pandas as pd
 
 def get_username(id):
     return GET_USER_NAME(id)
@@ -7,8 +8,34 @@ def get_username(id):
 def get_geral_information_and_finances(id):
     return GET_PROPOSTAS_BY_ID(id)
 
+# Inicializa os valores de data
+def initialize_data(id):
+
+    # Dicionário com dados de entrada
+    data = {
+        'generalFinances':pd.DataFrame(),
+        'financeDash':pd.DataFrame(),
+        'averageReviewHouseByArtist':pd.DataFrame(),
+        'ByOccurrence':pd.DataFrame(),
+        'downloadShowStatement':pd.DataFrame(),
+        'showStatement':pd.DataFrame(),
+        'weeklyFinances':pd.DataFrame(),
+        'artistRanking':pd.DataFrame(),
+        'reviewArtistByHouse':pd.DataFrame(),
+        'averageReviewArtistByHouse':pd.DataFrame(),
+        'reviewHouseByArtist':pd.DataFrame(),
+        'operationalPerformace':pd.DataFrame(),
+        'ByWeek':pd.DataFrame(),
+        'allOperationalPerformaceByOccurrenceAndDate':pd.DataFrame(),
+        'id':id,
+        'filterDate':None, 
+        'filterEstablishment':None
+    }
+
+    return data
+
 # Retorna os dados de cada dataframe num dicionário
-def get_dashbords_data(id, inputDate=None, inputEstablishment=None):
+def get_dashboards_data(data, id, inputDate=None, inputEstablishment=None):
     # pega dados e já aplica filtro se tiver
     generalFinances = apply_filter_in_dataframe(GET_WEEKLY_FINANCES(id), inputDate, inputEstablishment)
     financeDash = apply_filter_in_dataframe(GET_GERAL_INFORMATION_AND_FINANCES(id), inputDate, inputEstablishment)
@@ -50,3 +77,61 @@ def get_dashbords_data(id, inputDate=None, inputEstablishment=None):
     }
 
     return data
+
+def get_data_GeneralDash(data, id, inputDate=None, inputEstablishment=None):
+    generalFinances = apply_filter_in_dataframe(GET_WEEKLY_FINANCES(id), inputDate, inputEstablishment)
+    financeDash = apply_filter_in_dataframe(GET_GERAL_INFORMATION_AND_FINANCES(id), inputDate, inputEstablishment)
+    averageReviewHouseByArtist = apply_filter_in_dataframe(GET_AVAREGE_REVIEW_HOUSE_BY_ARTIST(id), inputDate, inputEstablishment)
+    ByOccurrence = apply_filter_in_dataframe(get_report_by_occurrence(GET_ALL_REPORT_ARTIST_BY_OCCURRENCE_AND_DATE(id).copy()), inputDate, inputEstablishment)
+    showStatement = apply_filter_in_dataframe(GET_PROPOSTAS_BY_ID(id), inputDate, inputEstablishment)
+    showStatement['DIA_DA_SEMANA'] = showStatement['DIA_DA_SEMANA'].apply(translate_day)
+    financeDash['DIA_DA_SEMANA'] = financeDash['DIA_DA_SEMANA'].apply(translate_day)
+    
+    data['generalFinances'] = generalFinances
+    data['financeDash'] = financeDash
+    data['averageReviewHouseByArtist'] = averageReviewHouseByArtist
+    data['ByOccurrence'] = ByOccurrence
+    data['showStatement'] = showStatement
+    
+    return data
+
+def get_data_Finances(data, id, inputDate=None, inputEstablishment=None):
+    weeklyFinances = apply_filter_in_dataframe( GET_WEEKLY_FINANCES(id), inputDate, inputEstablishment)
+    
+    data['weeklyFinances'] = weeklyFinances
+
+    return data
+
+def get_data_Review(data, id, inputDate=None, inputEstablishment=None):
+    artistRanking = apply_filter_in_dataframe(GET_ARTIST_RANKING(id), inputDate, inputEstablishment)
+    reviewArtistByHouse = apply_filter_in_dataframe(GET_REVIEW_ARTIST_BY_HOUSE(id), inputDate, inputEstablishment)
+    averageReviewArtistByHouse = apply_filter_in_dataframe(GET_AVAREGE_REVIEW_ARTIST_BY_HOUSE(id), inputDate, inputEstablishment)
+    reviewHouseByArtist = apply_filter_in_dataframe(GET_REVIEW_HOUSE_BY_ARTIST(id), inputDate, inputEstablishment)
+    
+    data['artistRanking'] = artistRanking
+    data['reviewArtistByHouse'] = reviewArtistByHouse
+    data['averageReviewArtistByHouse'] = averageReviewArtistByHouse
+    data['reviewHouseByArtist'] = reviewHouseByArtist
+    
+    return data
+
+def get_data_OperationalPerformace(data, id, inputDate=None, inputEstablishment=None):
+    allOperationalPerformaceByOccurrenceAndDate = apply_filter_in_dataframe(GET_ALL_REPORT_ARTIST_BY_OCCURRENCE_AND_DATE(id), inputDate, inputEstablishment)
+    operationalPerformace = apply_filter_in_dataframe(get_report_artist(allOperationalPerformaceByOccurrenceAndDate.copy()), inputDate, inputEstablishment)
+    ByWeek = apply_filter_in_dataframe(get_report_artist_by_week(allOperationalPerformaceByOccurrenceAndDate.copy()), inputDate, inputEstablishment)
+    
+    data['allOperationalPerformaceByOccurrenceAndDate'] = allOperationalPerformaceByOccurrenceAndDate
+    data['operationalPerformace'] = operationalPerformace
+    data['ByWeek'] = ByWeek
+    
+    return data
+
+def get_data_ShowStatement(data, id, inputDate=None, inputEstablishment=None):
+    downloadShowStatement = GET_PROPOSTAS_BY_ID(id)
+    
+    data['downloadShowStatement'] = downloadShowStatement
+    
+    return data
+
+
+
--- pages/Home.py
+++ pages/Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from utils.components import *
 from utils.functions import *
-from data.get_data import get_dashbords_data
+from data.get_data import initialize_data, get_data_GeneralDash, get_data_Finances, get_data_Review, get_data_OperationalPerformace, get_data_ShowStatement
 from utils.user import logout
 
-# resolver modularização
+# modularização das páginas
 from menu.general_dash import GeneralDashPage
 from menu.finances import FinancesPage
 from menu.reviews import ReviewPage
@@ -12,14 +12,13 @@ from menu.operational_performance import OperationalPerformacePage
 from menu.show_statement import ShowStatementPage
 
 st.set_page_config(
-            page_title="Relatórios Eshows",
-            page_icon="./assets/imgs/eshows-logo100x100.png",
-            layout="wide",
-        )
+    page_title="Relatórios Eshows",
+    page_icon="./assets/imgs/eshows-logo100x100.png",
+    layout="wide",
+)
 
 hide_sidebar()
 fix_tab_echarts()
-
 
 if 'loggedIn' not in st.session_state:
     st.switch_page("main.py")
@@ -29,7 +28,7 @@ if st.session_state['loggedIn']:
     user_name = st.session_state['user_data']["data"]['full_name']
 
     # Header
-    col1, col2, col3 = st.columns([9,0.7,1])
+    col1, col2, col3 = st.columns([9, 0.7, 1])
 
     # Caso não ache o ID de usuário
     if user_name is None:
@@ -37,12 +36,12 @@ if st.session_state['loggedIn']:
     else:
         col1.write(f"## Olá, {user_name}")
     col2.image("./assets/imgs/eshows100x100.png")
-    
-    col3.write('') # serve pra alinhar o botão
+
+    col3.write('')  # serve pra alinhar o botão
     if col3.button("Logout"):
         logout()
         st.switch_page("main.py")
-    
+
     # Nav
     st.divider()
     col4, empty, col5, col6 = st.columns([1, 1, 3, 2])
@@ -51,43 +50,41 @@ if st.session_state['loggedIn']:
     col5.markdown("<h3 style='text-align: center;'>Relatórios do Estabelecimento</h3>", unsafe_allow_html=True)
     with col6:
         inputEstablishment = filterEstablishmentComponent(user_id)
-    
-    # Pegando dados das querys
-    with st.spinner('Carregando dados, por favor aguarde.'):
-        try:
-            data = get_dashbords_data(user_id, inputDate, inputEstablishment)
-        except Exception as e:
-            st.error(f'Não foi possível carregar os dados, verifique a conexão. Erro: {e}')
-            data = None
 
+    data = initialize_data(user_id)
     # Body
-    tab1, tab2, tab3, tab4, tab5= st.tabs(["DASH GERAL", "FINANCEIRO", "AVALIAÇÕES", "DESEMPENHO OPERACIONAL", "EXTRATO DE SHOWS"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["DASH GERAL", "FINANCEIRO", "AVALIAÇÕES", "DESEMPENHO OPERACIONAL", "EXTRATO DE SHOWS"])
     with tab1:
         try:
+            data = get_data_GeneralDash(data, user_id, inputDate, inputEstablishment)
             page = GeneralDashPage(data)
             page.render()
         except Exception as e:
             st.error(f'Não foi possível carregar a página. Erro: {e}')
     with tab2:
         try:
+            data = get_data_Finances(data, user_id, inputDate, inputEstablishment)
             page = FinancesPage(data)
             page.render()
         except Exception as e:
             st.error(f'Não foi possível carregar a página. Erro: {e}')
     with tab3:
         try:
+            data = get_data_Review(data, user_id, inputDate, inputEstablishment)
             page = ReviewPage(data)
             page.render()
         except Exception as e:
             st.error(f'Não foi possível carregar a página. Erro: {e}')
     with tab4:
         try:
+            data = get_data_OperationalPerformace(data, user_id, inputDate, inputEstablishment)
             page = OperationalPerformacePage(data)
             page.render()
         except Exception as e:
             st.error(f'Não foi possível carregar a página. Erro: {e}')
     with tab5:
         try:
+            data = get_data_ShowStatement(data, user_id, inputDate, inputEstablishment)
             page = ShowStatementPage(data)
             page.render()
         except Exception as e:
